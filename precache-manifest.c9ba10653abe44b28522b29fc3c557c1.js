8a7629355dfcd9138c715a52ea95b571bbbe0b32
Chima Ezeoke <chima82x@gmail.com>
2019-07-08 18:35:14 +0100

    Updates

---
+++ precache-manifest.c9ba10653abe44b28522b29fc3c557c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29d176589e5b9e624b7d8b7363aca3fd",
+    "url": "/rolodex-monsters/index.html"
+  },
+  {
+    "revision": "d45842f7652188922ce5",
+    "url": "/rolodex-monsters/static/css/main.437a3e03.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/rolodex-monsters/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "d45842f7652188922ce5",
+    "url": "/rolodex-monsters/static/js/main.dbc2119f.chunk.js"
+  },
+  {
+    "revision": "b3f7ca6ebbd0d3992a76",
+    "url": "/rolodex-monsters/static/js/runtime~main.bc0c6299.js"
+  }
+]);
